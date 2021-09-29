246d85f7767c08f36b54bc02ebe2919b58c8852d
Alec Magnet <amagnet@gmail.com>
2021-09-28 22:50:20 -0400

    finished deliverables

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,25 @@
 export function manageFriends(state, action) {
   // your code here
+  // state = { friends: [] }
+  // action = {
+  //   type: "friends/add",
+  //   payload: {
+  //     name: "Chrome Boi",
+  //     hometown: "NYC",
+  //     id: 1
+  //   }
+  // }
+  // action = {
+  //   type: "friends/remove",
+  //   payload: 1,
+  // }
+
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] }
+    case "friends/remove":
+      return { friends: state.friends.filter((f) => f.id !== action.payload)}
+    default:
+      return state
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,14 @@
 export function managePresents(state, action) {
   // your code here
+  // state = { numberOfPresents: 0 }
+  // action = {
+  //   type: "presents/increase",
+  // }
+
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
